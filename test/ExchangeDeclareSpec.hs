98c90d9a8c1246997d6e49504db96d2f89f2c90b
Michael Klishin <michael@rabbitmq.com>
2013-10-12 21:46:41 +0400

    One more example for exchange.declare

--- test/ExchangeDeclareSpec.hs
+++ test/ExchangeDeclareSpec.hs
@@ -19,7 +19,25 @@ spec = do
         ch   <- openChannel conn
 
         _    <- declareExchange ch (newExchange {exchangeName = eName,
-                                                 exchangeType = "fanout"})
+                                                 exchangeType = "fanout",
+                                                 exchangeDurable = True})
+
+
+        _    <- declareExchange ch (newExchange {exchangeName    = eName,
+                                                 exchangePassive = True})
+
+        closeConnection conn
+
+    context "client-named, topic, non-durable, non-autodelete" $ do
+      it "declares the exchange" $ do
+        let eName = "haskell-amqp.topic.nd.na"
+
+        conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+        ch   <- openChannel conn
+
+        _    <- declareExchange ch (newExchange {exchangeName = eName,
+                                                 exchangeType = "topic",
+                                                 exchangeDurable = False})
 
 
         _    <- declareExchange ch (newExchange {exchangeName    = eName,
