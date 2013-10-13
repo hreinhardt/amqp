3ff479891e22ea289ba5a32ad220abbdc5f7a544
Michael Klishin <michael@rabbitmq.com>
2013-10-13 13:26:18 +0400

    Ditto

--- test/ExchangeDeclareSpec.hs
+++ test/ExchangeDeclareSpec.hs
@@ -10,37 +10,37 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "declareExchange" $ do
-    context "client-named, fanout, durable, non-autodelete" $ do
-      it "declares the exchange" $ do
-        let eName = "haskell-amqp.fanout.d.na"
+    describe "declareExchange" $ do
+        context "client-named, fanout, durable, non-autodelete" $ do
+            it "declares the exchange" $ do
+                let eName = "haskell-amqp.fanout.d.na"
 
-        conn <- openConnection "127.0.0.1" "/" "guest" "guest"
-        ch   <- openChannel conn
+                conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+                ch   <- openChannel conn
 
-        _    <- declareExchange ch (newExchange {exchangeName = eName,
-                                                 exchangeType = "fanout",
-                                                 exchangeDurable = True})
-
-
-        _    <- declareExchange ch (newExchange {exchangeName    = eName,
-                                                 exchangePassive = True})
-
-        closeConnection conn
-
-    context "client-named, topic, non-durable, non-autodelete" $ do
-      it "declares the exchange" $ do
-        let eName = "haskell-amqp.topic.nd.na"
-
-        conn <- openConnection "127.0.0.1" "/" "guest" "guest"
-        ch   <- openChannel conn
-
-        _    <- declareExchange ch (newExchange {exchangeName = eName,
-                                                 exchangeType = "topic",
-                                                 exchangeDurable = False})
+                _    <- declareExchange ch (newExchange {exchangeName = eName,
+                                                         exchangeType = "fanout",
+                                                         exchangeDurable = True})
 
 
-        _    <- declareExchange ch (newExchange {exchangeName    = eName,
-                                                 exchangePassive = True})
+                _    <- declareExchange ch (newExchange {exchangeName    = eName,
+                                                         exchangePassive = True})
 
-        closeConnection conn
+                closeConnection conn
+
+        context "client-named, topic, non-durable, non-autodelete" $ do
+            it "declares the exchange" $ do
+                let eName = "haskell-amqp.topic.nd.na"
+
+                conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+                ch   <- openChannel conn
+
+                _    <- declareExchange ch (newExchange {exchangeName = eName,
+                                                         exchangeType = "topic",
+                                                         exchangeDurable = False})
+
+
+                _    <- declareExchange ch (newExchange {exchangeName    = eName,
+                                                         exchangePassive = True})
+
+                closeConnection conn
