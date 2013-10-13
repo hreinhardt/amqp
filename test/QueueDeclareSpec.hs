df7123b7e557d8603d0d7d082ab04b7d6c781da5
Michael Klishin <michael@rabbitmq.com>
2013-10-13 13:27:46 +0400

    Ditto

--- test/QueueDeclareSpec.hs
+++ test/QueueDeclareSpec.hs
@@ -12,41 +12,41 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "declareQueue" $ do
-    context "client named, durable, non-autodelete, non-exclusive" $ do
-      it "declares the queue" $ do
-        let qName = "haskell-amqp.client-named.d.na.ne"
+    describe "declareQueue" $ do
+        context "client named, durable, non-autodelete, non-exclusive" $ do
+            it "declares the queue" $ do
+                let qName = "haskell-amqp.client-named.d.na.ne"
 
-        conn <- openConnection "127.0.0.1" "/" "guest" "guest"
-        ch   <- openChannel conn
+                conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+                ch   <- openChannel conn
 
-        (_, _, _) <- declareQueue ch (newQueue {queueName       = qName,
-                                                queueDurable    = True,
-                                                queueExclusive  = False,
-                                                queueAutoDelete = False})
+                (_, _, _) <- declareQueue ch (newQueue {queueName       = qName,
+                                                        queueDurable    = True,
+                                                        queueExclusive  = False,
+                                                        queueAutoDelete = False})
 
-        -- ensure the queue was declared
-        (_, _, _) <- declareQueue ch (newQueue {queueName = qName, queuePassive = True})
-        closeConnection conn
+                -- ensure the queue was declared
+                (_, _, _) <- declareQueue ch (newQueue {queueName = qName, queuePassive = True})
+                closeConnection conn
 
 
-    context "server- named, non-durable, non-autodelete, exclusive" $ do
-      it "declares the queue, providing access to the server-generated name" $ do
-        let qName = ""
+        context "server- named, non-durable, non-autodelete, exclusive" $ do
+            it "declares the queue, providing access to the server-generated name" $ do
+                let qName = ""
 
-        conn <- openConnection "127.0.0.1" "/" "guest" "guest"
-        ch   <- openChannel conn
+                conn <- openConnection "127.0.0.1" "/" "guest" "guest"
+                ch   <- openChannel conn
 
-        (q, _, _) <- declareQueue ch (newQueue {queueName       = qName,
-                                                queueDurable    = False,
-                                                queueExclusive  = True,
-                                                queueAutoDelete = False})
+                (q, _, _) <- declareQueue ch (newQueue {queueName       = qName,
+                                                        queueDurable    = False,
+                                                        queueExclusive  = True,
+                                                        queueAutoDelete = False})
 
-        (_, cn, mn) <- declareQueue ch (newQueue {queueName = q, queuePassive = True})
+                (_, cn, mn) <- declareQueue ch (newQueue {queueName = q, queuePassive = True})
 
-        (isPrefixOf "amq.gen" q) `shouldBe` True
-        -- consumer count, undelivered message count
-        cn `shouldBe` 0
-        mn `shouldBe` 0
+                (isPrefixOf "amq.gen" q) `shouldBe` True
+                -- consumer count, undelivered message count
+                cn `shouldBe` 0
+                mn `shouldBe` 0
   
-        closeConnection conn
+                closeConnection conn
