fb28c4c6e6878e0707176656bc71503dc2748d38
Michael Klishin <michael@rabbitmq.com>
2013-10-13 13:27:51 +0400

    Ditto

--- test/Runner.hs
+++ test/Runner.hs
@@ -8,13 +8,13 @@ import qualified ExchangeDeclareSpec
 
 main :: IO ()
 main = hspec $ do
-  -- connection.*
-  describe "ConnectionSpec"      ConnectionSpec.spec
-  -- channel.*
-  describe "ChannelSpec"         ChannelSpec.spec
-  -- queue.declare
-  describe "QueueDeclareSpec"    QueueDeclareSpec.spec
-  -- exchange.declare
-  describe "ExchangeDeclareSpec" ExchangeDeclareSpec.spec
-  -- queue.delete
-  describe "QueueDeleteSpec"     QueueDeleteSpec.spec
+    -- connection.*
+    describe "ConnectionSpec"      ConnectionSpec.spec
+    -- channel.*
+    describe "ChannelSpec"         ChannelSpec.spec
+    -- queue.declare
+    describe "QueueDeclareSpec"    QueueDeclareSpec.spec
+    -- exchange.declare
+    describe "ExchangeDeclareSpec" ExchangeDeclareSpec.spec
+    -- queue.delete
+    describe "QueueDeleteSpec"     QueueDeleteSpec.spec
