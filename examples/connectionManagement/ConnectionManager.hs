1b53de469a784a83c83c19c0e97fe7e0c872dcff
Andre Van Der Merwe <andre@andrevdm.com>
2022-05-16 17:54:54 +0200

    Overview docs

--- examples/connectionManagement/ConnectionManager.hs
+++ examples/connectionManagement/ConnectionManager.hs
@@ -1,3 +1,34 @@
+{-|
+Module      : ConnectionManager
+Description : Demo showing how AMQP connections and channels can be managed to deal with exceptions
+
+This module should demonstrate how to
+  - Handle an initial connection if the AMQP server is stopped or unreachable
+  - Handle an AMQP server being stopped or unreachable while executing
+  - Manage a pool of channels (using Data.Pool) so that you are not creating channels too often
+     see https://www.rabbitmq.com/production-checklist.html#apps-connection-churn
+  - Manage a single shared connection
+     see https://www.rabbitmq.com/production-checklist.html#apps-connection-management
+  - Automatically restart consumers (`consumeMsgs`) when the connection is restarted
+     see `addOnReconnect` and its use in the Main.hs demo
+
+
+Some notes on usage
+  - Use Network.AMQP version 0.22.1 or above as it contains a fix for a potential connection issue
+     https://github.com/hreinhardt/amqp/commit/ff169bbb8eec2d62efb4819f56f2c2d679ff4fc5
+     In general always use the latest version possible
+  - There are various settings in `Opts` that you can configure.
+     E.g. Changing the pool size and how long to keep a channel open
+  - Control.Exception.Safe is used to avoid catching exceptions that should not be handled by user code
+     See https://hackage.haskell.org/package/safe-exceptions
+  - Read the Hackage docs esp the section on "Exceptions in the callback".
+  - Data.Pool does most of the heavy lifting for managing the channels
+
+
+Please note that this is an example and should not be considered production ready as-is.
+This code has not undergone the same testing as the core modules.
+-}
+
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -11,7 +42,6 @@ module ConnectionManager
     , closeHandle
     , defaultOpts
     , mkHandle
-    , retryUntilConnectionClose
     , withChannel
     , withConnection
     ) where
@@ -29,31 +59,36 @@ import Data.Time (NominalDiffTime)
 import           Safe (atMay, lastMay)
 
 
+-- | Connection options
 data Opts = Opts
-  { opKeepChannelOpenSeconds :: !NominalDiffTime
-  , opMaxPooledChannels :: !Int
-  , opRetyConnectionBackoffMilliseconds :: ![Int]
-  , opRetyConnectionMaxAttempts :: !(Maybe Int)
-  , opChanCreateFn :: !(Maybe (Rb.Channel -> IO ()))
+  { opKeepChannelOpenSeconds :: !NominalDiffTime     -- ^ Amount of time for which an unused resource is kept open. The smallest acceptable value is 0.5
+  , opMaxPooledChannels :: !Int                      -- ^ Maximum number of channels to keep open. The smallest acceptable value is 1.
+  , opRetyConnectionBackoffMilliseconds :: ![Int]    -- ^ milliseconds to delay between connection attempts
+  , opRetyConnectionMaxAttempts :: !(Maybe Int)      -- ^ How many times to retry connection. Nothing = never
+  , opChanCreateFn :: !(Maybe (Rb.Channel -> IO ())) -- ^ Function to run on each new connection, e.g. setting qos
   }
 
 
 defaultOpts :: Opts
 defaultOpts =
   Opts
-    { opKeepChannelOpenSeconds = 60 -- ^ Amount of time for which an unused resource is kept open. The smallest acceptable value is 0.5
-    , opMaxPooledChannels = 5 -- ^ Maximum number of channels to keep open. The smallest acceptable value is 1.
-    , opRetyConnectionBackoffMilliseconds = [100, 250, 475, 813, 1_319, 2_078, 3_217, 4_926] -- ^ milliseconds to delay between connection attempts
-    , opRetyConnectionMaxAttempts = Just 200 -- ^ How many times to retry connection. Nothing = never
-    , opChanCreateFn = Nothing -- ^ Function to run on each new connection, e.g. setting qos
+    { opKeepChannelOpenSeconds = 60
+    , opMaxPooledChannels = 5
+    , opRetyConnectionBackoffMilliseconds = [100, 250, 475, 813, 1_319, 2_078, 3_217, 4_926]
+    , opRetyConnectionMaxAttempts = Just 200
+    , opChanCreateFn = Nothing
     }
 
 
 
--- Combined connection and pool, makes it easier to ensure the pool is not used while the connection is being recreated etc
+-- Combined connection and pool
+-- This makes it easier to ensure the pool is not used while the connection is being recreated etc
+-- i.e. a pool is associated with a single open connection
 newtype Mcp = Mcp (Mv.MVar (Rb.Connection, Pl.Pool Rb.Channel))
 
--- | Opaque handle representing a rabbit connection
+
+-- | Opaque handle representing a AMQP connection
+-- This is the type that the user will use rather than the normal Network.AMQP.Connection
 data Handle = Handle
   { hMcp :: !Mcp -- ^ Connection and it's pool of channels
   , hWithConnection :: !(forall a. Handle -> (Rb.Connection -> IO a) -> IO a) -- ^ Helper method for the public withConnection function
@@ -82,7 +117,9 @@ mkHandle ropts copts = do
     initMcp :: Handle -> IO Handle
     initMcp h = do
       let (Mcp mcp) = hMcp h
+      -- Create a new channel pool for the new connection
       pool <- Pl.createPool (createChan h) Rb.closeChannel 1 (opKeepChannelOpenSeconds copts) (opMaxPooledChannels copts)
+      -- Create a new connection
       conn <- newRabbitConnection h
       Mv.putMVar mcp (conn, pool)
       pure h
@@ -92,13 +129,18 @@ mkHandle ropts copts = do
     createChan h = do
       let (Mcp mcp) = hMcp h
       (c, _) <- Mv.readMVar mcp
+      -- Create a new channel
+      -- Optionally call opChanCreateFn if it is not Nothing
+      -- Call addChannelExceptionHandler so we can manage any channel exceptions
       manageRabbitException
         h
         (do
           ch <- Rb.openChannel c
+
           case opChanCreateFn copts of
             Nothing -> pure ()
             Just f -> f ch
+
           Rb.addChannelExceptionHandler ch (onRabbitException h)
           pure ch
         )
@@ -106,10 +148,14 @@ mkHandle ropts copts = do
 
     onRabbitException :: Handle -> SomeException -> IO a
     onRabbitException h ex =
+      -- Use `isNormalChannelClose` to see if the channel was closed normally or if there was an error
+      -- See the docs for `isNormalChannelClose` for details
       if Rb.isNormalChannelClose ex
         then
+          -- Normal close, so rethrow so that the normal shutdown can occur
           throwM ex
         else do
+          -- Unexpected exception. Assume that the connection will is terminated and start a new one.
           restartConnection h
           throwM ex
 
@@ -117,8 +163,13 @@ mkHandle ropts copts = do
     restartConnection h = do
       let (Mcp mcp) = hMcp h
       (_conn, pool) <- Mv.takeMVar mcp
+
+      -- Try to close any open channels from the old pool
       Pl.destroyAllResources pool
+      -- New connection and pool
       _ <- initMcp h
+
+      -- Run all the actions that the user wanted to happen after a reconnect
       rs <- Tv.readTVarIO $ hOnReconnect h
       sequenceA_ rs
 
@@ -131,12 +182,17 @@ mkHandle ropts copts = do
     withConnection' :: Handle -> (Rb.Connection -> IO a) -> IO a
     withConnection' h@(Handle (Mcp cp') _ _) fn = do
       (conn, _pool) <- Mv.readMVar cp'
+      -- Run the user function with a connection
+      -- Any exception in the user function is assumed to be problem with the connection
+      -- The user should only run functions that actually need the connection in the callback
       manageRabbitException h (fn conn)
 
 
     newRabbitConnection :: Handle -> IO Rb.Connection
     newRabbitConnection h = do
+      -- Get a new connection, possibly waiting for the server
       conn <- tryNewRabbitConnection (fromMaybe 0 $ opRetyConnectionMaxAttempts copts) 0
+      -- Add a handler to watch for connection closes
       Rb.addConnectionClosedHandler conn True (restartConnection h)
       pure conn
 
@@ -151,16 +207,21 @@ mkHandle ropts copts = do
             then throwM ex
           else do
             let
+              -- Get the back off values
               backoff = opRetyConnectionBackoffMilliseconds copts
               -- Largest possible back off, default of 200ms if none was set
               largestBackoffMs = fromMaybe 200 . lastMay $ backoff
               -- Delay for current number of retries
               delay = 1_000 * fromMaybe largestBackoffMs (atMay backoff atRetry)
 
+            -- Delay
             threadDelay delay
+            -- Try again
             tryNewRabbitConnection (retriesLeft - 1) (atRetry + 1)
          )
 
+
+-- | Close the handle's connection and channels
 closeHandle :: Handle -> IO ()
 closeHandle (Handle (Mcp mcp) _ _) =
   Mv.tryTakeMVar mcp >>= \case
@@ -168,26 +229,20 @@ closeHandle (Handle (Mcp mcp) _ _) =
     Just (conn, _) -> Rb.closeConnection conn
 
 
+-- | Perform an operation with a pooled channel. Keep this callback as small as possible, only functions that actually need a channel
 withChannel :: Handle -> (Rb.Channel -> IO a) -> IO a
 withChannel (Handle (Mcp mcp) _ _) fn = do
   (_, pool) <- Mv.readMVar mcp
   Pl.withResource pool fn
 
 
+-- | Perform an operation with the connection. Keep this callback as small as possible, only functions that actually need the connection
 withConnection :: Handle -> (Rb.Connection -> IO a) -> IO a
 withConnection h@(Handle _ w _) = w h
 
 
-retryUntilConnectionClose :: Int -> IO a -> IO a
-retryUntilConnectionClose retrySecs fn =
-  catch fn handleEx
-
-  where
-    handleEx (_::SomeException) = do
-      threadDelay retrySecs
-      retryUntilConnectionClose retrySecs fn
-
-
+-- | Add a callback to run when the connection is reconnected
+-- Note this does not run the callback immediately only on reconnect
 addOnReconnect :: Handle -> IO () -> IO ()
 addOnReconnect (Handle _ _ rs') fn =
   atomically . Tv.modifyTVar' rs' $ \rs -> fn : rs
--- examples/connectionManagement/ExampleConnectionManagement.hs
+++ examples/connectionManagement/ExampleConnectionManagement.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Main
+Description : Example showing error handling
+
+This example uses the demo ConnectionManager module to handle connection and channel errors.
+You should be able to start this demo (`cabal run amqp-example-connection-management`) before
+RabbitMQ is running, terminate RabbitMQ while it is running etc and it should continue to
+work.
+
+See the ConnectionManager module for an explanation of the implementation.
+-}
+
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -7,7 +19,7 @@ module Main
 
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           Control.Concurrent (threadDelay)
-import           Control.Monad (void, (>=>))
+import           Control.Monad (void)
 import qualified Data.Time as Dt
 import           Network.AMQP ( Ack(..)
                               , DeliveryMode(..)
@@ -31,41 +43,88 @@ import           Network.AMQP ( Ack(..)
 
 import qualified ConnectionManager as Cm
 
-
+-- | Example showing the use of ConnectionManager to manage connection and channel failure
 main :: IO ()
 main = do
+  -- Use `mkHandle` rather than `openConnection`
+  -- This will create a "handle" that you can use to safely get the connection and a channel
   cm <- Cm.mkHandle (fromURI "amqp://guest:guest@localhost:5672") Cm.defaultOpts
 
-  Cm.withConnection cm $ getServerProperties >=> print
+  -- Example of using the connection to get the server properties
+  Cm.withConnection cm $ \conn -> do
+    props <- getServerProperties conn
+    print props
 
+
+  -----------------------------------------------------------------------------------------------------------
+  -- Setup --
+  --
+  -- Setup for the consumer and producer
+  -- Using `withChannel` to get a channel from the pool.
+  --
+  -- This is otherwise very similar to ExampleConsumer/ExampleProducer
+  -- NB do as little work in withChannel as possible. You want to use the channel
+  -- and return it to the pool so that it can be used elsewhere.
+  -----------------------------------------------------------------------------------------------------------
   Cm.withChannel cm $ \chan -> do
     void $ declareQueue chan newQueue {queueName = "myQueueEN"}
     declareExchange chan newExchange {exchangeName = "topicExchg", exchangeType = "topic"}
     bindQueue chan "myQueueEN" "topicExchg" "en.*"
+  -----------------------------------------------------------------------------------------------------------
 
+
+  -----------------------------------------------------------------------------------------------------------
+  -- Consumer --
+  --
+  -- If the connection is killed then any handler started by `consumeMsgs` will no longer be running.
+  --
+  -- The ConnectionManager has an `addOnReconnect` function that lets you run your code when a
+  -- reconnect succeeds.
+  -----------------------------------------------------------------------------------------------------------
   let runConsumer =
+        -- The call to `consumeMsgs` that we want to keep running
         Cm.withChannel cm $ \chan -> do
          void $ consumeMsgs chan "myQueueEN" Ack myCallbackEN
 
 
+  -- Start automatically of the connection is stopped
   Cm.addOnReconnect cm runConsumer
+  -- And start the consumer right now as well
   runConsumer
+  -----------------------------------------------------------------------------------------------------------
 
+
+  -----------------------------------------------------------------------------------------------------------
+  -- Producer
+  --
+  -- Run the producer demo loop
+  -----------------------------------------------------------------------------------------------------------
   runProducer cm 0
+  -----------------------------------------------------------------------------------------------------------
+
+
+  -- This will never get called in this demo
   Cm.closeHandle cm
 
 
 
+-- | Callback for the consumer
 myCallbackEN :: (Message, Envelope) -> IO ()
 myCallbackEN (msg, env) = do
   putStrLn $ "received from EN: " <> BL.unpack (msgBody msg)
   ackEnv env
 
 
+
+-- | Producer loop
+--
+-- This loop will try to publish a message once a second
 runProducer :: Cm.Handle -> Int -> IO ()
 runProducer cm count = do
   dt <- Dt.getCurrentTime
+
   -- Do as little work in withChannel or withConnection as possible
+  -- Notice that `getCurrentTime` and esp the threadDelay are called before/after withChannel
   Cm.withChannel cm $ \chan ->
     void $ publishMsg
              chan
@@ -76,7 +135,7 @@ runProducer cm count = do
                       }
               )
 
-  -- NB don't block in the withChannel
+  -- NB don't block in the withChannel call
   threadDelay 1_000_000
   runProducer cm (count + 1)
 
